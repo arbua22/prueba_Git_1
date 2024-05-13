be53dd8d519e4966a02c1553c490379709031c97
Luis <Luis@ArbusPC>
2024-05-14 01:11:50 +0200

    Ejemplo Uso Netbeans 1

---
+++ src/main/java/com/mycompany/ana_420/Ana_420.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.ana_420;
+
+/**
+ *
+ * @author Luis
+ */
+public class Ana_420 {
+
+    public static void main(String[] args) {
+
+        fumar();
+
+    }
+
+    public static void fumar() {
+
+        System.out.println("Ana: *Smoking* pasae el mechero arbu");
+
+    }
+
+}
